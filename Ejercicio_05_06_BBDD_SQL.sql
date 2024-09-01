6bd6bdc4ebd9426bfcd6e170db1e4736408c4658
AndreaGonzalezG <mariaandreagg@gmail.com>
2024-09-01 21:25:37 +0200

    actualizacion ejercicios API, SQL

--- Ejercicio_05_06_BBDD_SQL.sql
+++ Ejercicio_05_06_BBDD_SQL.sql
@@ -62,7 +62,7 @@ ADD COLUMN talla INT NOT NULL;
 
 -- Correción errores tabla empleados
 ALTER TABLE empleados
-MODIFY COLUMN salario FLOAT NOT NULL DEFAULT 0;
+MODIFY COLUMN salario FLOAT DEFAULT NULL;
 
 -- Correción errores tabla clientes
 ALTER TABLE clientes
@@ -72,34 +72,22 @@ DROP COLUMN pais;
 ALTER TABLE facturas
 ADD COLUMN total FLOAT;
 
--- 2. INSRETAR DATOS EN TABLAS
+-- 2. INSERTAR DATOS EN TABLAS
 -- Tabla zapatillas
 INSERT INTO zapatillas (id_zapatilla, modelo, color, marca, talla)
-VALUES (1, 'XQYUN', 'Negro', 'Nike', 42), 
-		(2, 'UOPMN', 'Rosas', 'Nike', 39), 
-		(3, 'OPNYT', 'Verdes', 'Adidas', 35);
+VALUES (1, 'XQYUN', 'Negro', 'Nike', 42), (2, 'UOPMN', 'Rosas', 'Nike', 39), (3, 'OPNYT', 'Verdes', 'Adidas', 35);
 
 -- Tabla empleados
 INSERT INTO empleados (id_empleado, nombre, tienda, salario, fecha_incorporacion)
-VALUES (1, 'Laura', 'Alcobendas', 25987, '2010-09-03'), 
-		(2, 'María', 'Sevilla', DEFAULT,'2001-04-11'), 
-        (3, 'Ester', 'Oviedo', 30165.98, '2000-11-29');
-        
-DELETE FROM empleados
-WHERE salario IS NULL;
-
+VALUES (1, 'Laura', 'Alcobendas', 25987, '2010-09-03'), (2, 'María', 'Sevilla','','2001-04-11'), (3, 'Ester', 'Oviedo', 30165.98, '2000-11-29');
 
 -- Tabla clientes
 INSERT INTO clientes (id_cliente, nombre, numero_telefono, email, direccion, ciudad, provincia, codigo_postal)
-VALUES (1, 'Monica', '1234567289', 'monica@email.com', 'Calle Felicidad', 'Móstoles', 'Madrid', '28176'), 
-		(2, 'Lorena', '289345678', 'lorena@email.com', 'Calle Alegria', 'Barcelona', 'Barcelona', '12346'), 
-        (3, 'Carmen', '298463759', 'carmen@email.com', 'Calle del Color', 'Vigo', 'Pontevedra', '23456');
+VALUES (1, 'Monica', '1234567289', 'monica@email.com', 'Calle Felicidad', 'Móstoles', 'Madrid', '28176'), (2, 'Lorena', '289345678', 'lorena@email.com', 'Calle Alegria', 'Barcelona', 'Barcelona', '12346'), (3, 'Carmen', '298463759', 'carmen@email.com', 'Calle del Color', 'Vigo', 'Pontevedra', '23456');
 
 -- Tabla facturas
 INSERT INTO facturas (id_factura, numero_factura, fecha, id_zapatilla, id_empleado, id_cliente, total)
-VALUES (1, '123', '2001-12-11', 1, 2, 1, 54.98), 
-		(2, '1234', '2005-05-23', 1, 1, 3, 89.91), 
-        (3, '12345', '2015-09-18', 2, 3, 3, 76.23);
+VALUES (1, '123', '2001-12-11', 1, 2, 1, 54.98), (2, '1234', '2005-05-23', 1, 1, 3, 89.91), (3, '12345', '2015-09-18', 2, 3, 3, 76.23);
 
 -- 3. ACTUALIZACIÓN DE ERRORES
 -- Tabla zapatillas
